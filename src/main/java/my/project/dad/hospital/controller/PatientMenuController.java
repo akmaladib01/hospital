74407fa311e42073c8d92fbb2a93c9975f377fae
akmaladib01 <akmaladib15801@gmail.com>
2023-06-19 15:47:04 +0800

    upudate

--- src/main/java/my/project/dad/hospital/controller/PatientMenuController.java
+++ src/main/java/my/project/dad/hospital/controller/PatientMenuController.java
@@ -10,9 +10,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.client.RestTemplate;
 
 import my.project.dad.hospital.model.Patient;
@@ -81,9 +80,9 @@ public class PatientMenuController {
 	}
 	
 	/**
-	 *This method gets an order type
+	 *This method gets a patient
 	 *
-	 *@param orderTypeld
+	 *@param patient_id
 	 *@param model
 	 *@return
 	 */
@@ -94,13 +93,13 @@ public class PatientMenuController {
 		String pageTitle = "New Patient";
 		Patient patient = new Patient();
 
-		// This block get an order type to be updated
+		// This block gets a patient to be updated
 		if (patient_id > 0) {
 
-			// Generate new URI and append orderTypeld to it
+			// Generate new URI and append patient_id to it
 			String uri = defaultURI + "/" + patient_id;
 
-			// Get an order type from the web service
+			// Get a patient from the web service
 			RestTemplate restTemplate = new RestTemplate();
 			patient = restTemplate.getForObject(uri, Patient.class);
 
@@ -108,7 +107,7 @@ public class PatientMenuController {
 			pageTitle = "Edit Patient";
 		}
 
-		// Attach value to pass to front end
+		// Attach values to pass to the front end
 		model.addAttribute("patient", patient);
 		model.addAttribute("pageTitle",pageTitle);
 
@@ -116,9 +115,9 @@ public class PatientMenuController {
 	}
 	
 	/**
-	*This method deletes an order type
+	*This method deletes a patient
 	*
-	* @param orderTypeld
+	* @param patient_id
 	* @return
 	*/
 	@RequestMapping("/patient/delete/{patient_id}")
@@ -127,11 +126,10 @@ public class PatientMenuController {
 		// Generate new URI, similar to the mapping in OrderTypeRESTController
 		String uri = defaultURI + "/{patient_id}";
 
-		// Send a DELETE request and attach the value of orderTypeId into URI
+		// Send a DELETE request and attach the value of patient_id into URI
 		RestTemplate restTemplate = new RestTemplate();
-		restTemplate.delete(uri,Map.of("patient_id", Integer.toString(patient_id)));
+		restTemplate.delete(uri, Map.of("patient_id", Integer.toString(patient_id)));
 
 		return "redirect:/patient/list";
-
 	}
 }
